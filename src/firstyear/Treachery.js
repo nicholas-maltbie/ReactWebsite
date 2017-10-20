6ac43b478b7df7072915b0d4dbe475254cc47b7c
Nick Maltbie <nick.dmalt@gmail.com>
2017-10-19 21:53:38 -0400

    Added treachery page

---
+++ src/firstyear/Treachery.js
@@ -0,0 +1,190 @@
+import React, { Component } from 'react';
+import { Project, Cover, EmptyPadding } from '../Components';
+import { HashLink } from 'react-router-hash-link';
+import '../App.css';
+import img from "../media/Margam_Castle.jpg";
+
+const Treachery = function () {
+  return (
+    <div>
+      <Cover image={img} height="100%">
+
+        <EmptyPadding height='20vh'/>
+
+        <div className='row center-block White-box'>
+          <div className='text-dump col-sm-offset-1 col-sm-10 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6  col-xs-12'>
+            <h1 align='center'>Treachery Honors Experience</h1>
+            <h3 align='center'>(Summer 2017)</h3>
+
+            <p>
+              Treachery is a project that I have started on to make a video
+              game/interactive experience. This project is inspired by the board
+              game betrayal at house on the hill made by Avalon Hill. I am
+              working on the project as part of a group and our we are using
+              github to track and manage our work. Our <a href="https://github.com/nicholas-maltbie/treachery">project page </a>
+              has more information on the project.
+            </p>
+            <p>
+              The object of this experience it to manage a large project as a group
+              and to learn how to make and design 3D models. This experience will
+              be over this August but the project will always need more work
+              to be done in order to finish the full game.
+            </p>
+            <p>
+              The project was planned to last from April to August 2017. The project
+              took the time span. The project was not able to be completed in
+              the current time frame, although I was able to make an alpha version
+              of the project.
+            </p>
+            <p>
+              The project is fully published to github and I have made posts on
+              the <a href="https://forum.unity3d.com/threads/wip-treachery-horror-multiplayer-game.484787/">Unity Forums </a>
+              sharing the project with any interested people. Other people can use
+              anything from the project under the GPL v3 License. This is an open
+              source license where other people can use the material for free with
+              some caveats. You can read more about this on the project's
+              <a href="https://github.com/nicholas-maltbie/Treachery/blob/master/LICENSE.txt">license page </a>
+            </p>
+          </div>
+        </div>
+
+        <EmptyPadding height='10vh'/>
+
+        <div className='row center-block White-box'>
+          <div className='text-dump col-sm-offset-1 col-sm-10 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6  col-xs-12'>
+            <h2 align='center'>Final Progress</h2>
+            <p>
+              The game is far from complete but does support a basic version of the
+              game. Below is a screenshot from the game being played. In the picture
+              players are simply exploring the house. In the game, players explore
+              the house, and after a certain point the 'haunt' will start and
+              one player will become the traitor. This traitor will attempt to
+              kill the other 'heroes' while the heroes need to complete an
+              objective to stop the traitor. There is simply a basic implementation
+              of this completed in the project. I really enjoyed learning more about
+              all the tools I used to create this and hope they will remain useful
+              in the future.
+            </p>
+
+            <img alt="Picture of gameplay"
+            src="https://lh3.googleusercontent.com/2CA0ZKnhf1GY_p2gXktgwktRcYsxvOJv-gEMNOQrJvqKrspYxEwWJ2hJLNH04YkQiOmKqTpzHxg7pqJfhsZkQ3zyMGHOAcUrrka4hPxCFEH6FkrK5ig81mSQH46Shq1Pt5IO2uvUtxDl3Pp8euliSrun5mVRLb3BpCBS5OrwFohfUDe1ixdzwno0QHY2HjsLJ2NbTmyP1JOVmu2xX0ROUencqP8_9scTxGQjnhUu2cx5_zqrVRZaZ-gGyn3eGKktjuhItiuib8gYGtC7ZD20yWAxVbSZAnQaflI1X5ZvqSGLUtQ4WCy3LbCz7OhgsfvewGY4NCVRD6KNIg5J4-2etxIebRpZvHR9m7KqFb_779iysFoFCU_G_xBKgVmcfxQzoZW1DFJ95VP-Vk38itXyyGt-7ET2eZE_j55hK8cb9HcxjsMR0P4zfzqzkfSEuMewkJst_taoZ-ugsRq7Q_so6eJOgsH0VtxUCZsAFwau1OFqtRvPfUp4-56eDfgZncCjApWkcM_0b1dDjorKHYtYA29_HHk7DwKqoWvfz8yet3ROjFP_LNCPf9B4vmiK51UqqYQZdX7ZjXy67LmUhvtWZgNs9Cfw1o9LeKGBDHsm3wcltx3FCzFF3xK_lYirgbLgPONfGRqvdBYfxG8xLQK4ItHV017tmU6gz3F2PDPquJ8t9Q=w1725-h966-no"
+            className="Image-content"/>
+            <p>
+              Picture of exploring the house with a 'friend'.
+            </p>
+
+            <img alt="Picture of gameplay (2)"
+            src="https://lh3.googleusercontent.com/U_MGBASU8nzi1BPeo-dwe7FfsCXz-1cfpOwS2SGao6eHPSJfwC8L9pwu_-Bfs5kY6o4vp5mIOH2525LQC0aR2yLGS2-OS4-AtgJNTYgn9gH8T2uGI80AS7FkFn9qcv7Bnwuu9y-r8BBRBXmyHAWeBLzOaVqxt5sWPoncGt06-JNJmU7bm08suCY639uc5B_ksfjUvSAMkC0K0VFbYQ7p2zN6CPqsQPS1d-LFuvgKCHChuFYQZjcW7reBOi6dpEj1RDob_eTPEuV0Wv0RRIzkLCw-wmF66RPxK0t0uV9Wy5lHZOgauKSUV_i-Pp1EUEOJax8ukARwgW5ppfEGl_C1dg9p7WWBqnpI5gztKu7TVXcUyYSalKzWWaZMw23KzhZoA_u_-ClP13VjlNOrlFAlknrpYBR0AZRfBdlq9-9-p76buP3cjmoRUJDyfazWbV1GMjtG5bxVOK7ERn5AYTJPDOTpAr57X-7B-cZjIlIO_XUg3qWOg3k74uvy_AfsCsQ4lgGoVXXNWK5EULgY0JjPMZssxa_nZRtH8VpbLR9zcMs9yIh3Sf9p01w-_89mnaa9daBjZamqDi2mWiM_K-2pyYXDyOg6yOruXRdxdaXbBbuexHNGDhowzXq8wSVDR2y_eFGKMFTSELZz4AtkQfrZgDquXhsEVm1qVE8PAp_Q7xSfKQ=w1718-h966-no"
+            className="Image-content"/>
+            <p align="center">
+              Picture of main room.
+            </p>
+          </div>
+        </div>
+
+        <EmptyPadding height='10vh'/>
+
+        <div className='row center-block White-box'>
+          <div className='text-dump col-sm-offset-1 col-sm-10 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6  col-xs-12'>
+            <h2> Game Explanation </h2>
+            <p>
+              1) Exploration - Players explore the house, find items and discover
+              more information.
+            </p>
+            <p>
+              2) Haunt - At some point in the game, something will trigger a haunt
+              (finding an omen, discovering a secret, etc...) and one player will
+              become the traitor and want to kill everyone else in a special scenario.
+            </p>
+            <p>
+              In the board game, there are 50 unique haunts that vary from one
+              traitor, free for all, cooperative, and mixed. As of right now we
+              have only implemented one proof of concept haunt, the Ring Haunt:
+              it starts when a player picks up the ring (any number of haunts is
+              supported).
+            </p>
+          </div>
+        </div>
+
+        <EmptyPadding height='10vh'/>
+
+        <div className='row center-block White-box'>
+          <div className='text-dump col-sm-offset-1 col-sm-10 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6  col-xs-12'>
+            <h2 align="center">Reflection</h2>
+            <p>
+              The project had a vast scope and I really should have limited my
+              original vision. My ambition and drive when working on the project
+              were far beyond my actual ability. This passion helped push the
+              project forward in the first month but as I ran out of time during
+              the summer due to my job I faced major setbacks. In order to deal
+              with this lack of energy, as a group, we started hosting the project
+              on Github. This allowed us to remotely track our progress and sync
+              our work as a group. The scope of the project was also decreased to
+              just get an alpha version of the game working. These adaptations to
+              challenge and adversity had somewhat crushed my original enthusiasm
+              as the previous ideas no longer worked; I never enjoy admitting that
+              I am wrong and need to change. Instead of spending my time designing
+              and creating new applications I found myself more often than not
+              debugging, writing reports and slowly progressing to the next
+              objective. Progress was consistent but not what I had initially
+              expected.
+            </p>
+            <p>
+              Despite not getting as much completed as I would have liked to, I
+              developed my skills using the different software in addition to my
+              skills of working in a group. I had previously hardly heard of Blender
+              but after months of effort I learned how to make and animate a
+              humanoid character. The character is used in the current version of
+              the game and is one of the aspects my friends like most about the
+              project; the character is definitely not a perfect but I can proudly
+              look at the results from this and think that i have learned something.
+              Additionally, I wouldn't have been able to make this character by
+              myself. I had communicate and show progress reports to my friends.
+              We worked together on the initial design and planning. I then took
+              the results from this and made some initial attempts. At a later
+              meeting I showed the results to the group, it was far from perfect
+              but from these conversations I was able to continually improve the
+              character. After about two months I had fully incorporated the
+              character and it worked extremely well with the final project. In
+              future group projects I hope to approach difficult challenges in this
+              manner of using communication and conversations to improve the final
+              project.
+            </p>
+            <p>
+              Over the summer, when majority of the work was completed on the project,
+              I also had a job doing research at Children’s Hospital. This took
+              away most of my free time and made it almost impossible to manage my
+              time. I had to constantly make decisions about what to spend my time
+              on. I defaulted to working at my job as this had more direct professional
+              implications but still managed to report my progress to my group (even
+              if it was very little) and continued to develop next steps and managed
+              the current progress. One consistent point of difficult between me
+              and my group was the level of dedication to the project. I have to
+              accept that other people may not be as interested in the project and
+              have to act toward them accordingly. I specifically remember giving
+              different examples such as rendered images as opposed to gameplay
+              segments when presenting progress to different group members. I also
+              did this at my job when attempting to ask for help from different
+              co-workers, like giving images and graphs to my boss when presenting
+              progress and showing code snippets to my more tech savvy co workers.
+              If I am able to be adaptive and willing to put in extra effort I will
+              get better results when working in a group environment. This
+              experience and my reflection will provide me with a greater perspective.
+              In the future when I am working in any level of professional capacity I
+              will better understand how to communicate and adapt myself to get the
+              best results from working as a group.
+            </p>
+          </div>
+        </div>
+
+        <EmptyPadding height='15vh'/>
+
+        <div className='row'/>
+      </Cover>
+
+    </div>
+
+  )
+}
+
+export default Treachery
