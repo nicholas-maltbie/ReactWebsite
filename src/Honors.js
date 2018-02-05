e58c918e7b053e53c8eef6c1233978afa68454b1
Nicholas Maltbie <nick.dmalt@gmail.com>
2018-02-04 18:03:31 -0800

    Added stair page

--- src/Honors.js
+++ src/Honors.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import bg from './media/IMG_4169-EFFECTS.jpg';
 import lamp from './media/icons/treachery.png';
+import stair from './media/icons/stairIcon.png';
 import bearcat from './media/icons/bearcatCoders.png';
 import uhp from './media/icons/UHP.png';
 import './App.css';
@@ -11,6 +12,7 @@ import Treacery from './firstyear/Treachery'
 import BCat from './firstyear/BearcatCoders'
 import Gateway from './firstyear/Gateway'
 import YearOneReview from './firstyear/yearinreview'
+import Knee from './secondyear/Knee'
 
 class Honors extends Component {
   render() {
@@ -18,6 +20,7 @@ class Honors extends Component {
       <div>
         <Route exact path="/UHP" component = { List } />
         <Route path="/UHP/treachery" component = { Treacery }/>
+        <Route path="/UHP/knee" component = { Knee }/>
         <Route path="/UHP/BearcatCoders" component = { BCat }/>
         <Route path="/UHP/intro" component = { Gateway }/>
         <Route path="/UHP/FirstReview" component = { YearOneReview }/>
@@ -37,6 +40,20 @@ const List = function ({}) {
     <h1 className="text-center center-block col-lg-12">
       Honors
     </h1>
+
+    <h2 className="text-center center-block col-lg-12">
+      Second Year
+    </h2>
+    
+    <div className="col-lg-12" style={rowstyle}>
+      <Project title="Accessibility Project" date="(Fall/Spring 2017,2018)"
+        link="/UHP/Knee" icon={stair} iconalt="Treachery Lamp Icon">
+        In fall of last year, I tore my meniscus doing everyday activities
+        and needed to get surgery to repair the injury. This experinece is me 
+        reflecting upon the experience and the effect it has had on me. 
+      </Project>
+      
+    </div>
 
     <h2 className="text-center center-block col-lg-12">
       First Year
