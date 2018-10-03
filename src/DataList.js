d43d872398966c47904211a198e08e43c42f95eb
Nick Maltbie <nick.dmalt@gmail.com>
2018-10-03 13:59:10 -0400

    Added honors projects to activity feed and changed honors page to use DataList

--- src/DataList.js
+++ src/DataList.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import { Project } from './Components';
 import './App.css';
 
+var all_data = require('./activity-feed.json')
+
+console.log(all_data.map(proj => proj.title))
+all_data.sort(function(proj1, proj2) {
+  var elems = ["startYear", "endYear", "startMonth", "endMonth"]
+  for (var idx = 0; idx < elems.length; idx++) {
+    var elem = elems[idx]
+    if (proj1[elem] !== proj2[elem])
+      return proj2[elem] - proj1[elem]
+  }
+  return 0
+})
+
+console.log(all_data.map(proj => proj.title))
+
 var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 function numberRange (start, end) {
@@ -24,20 +39,36 @@ class DataList extends Component {
 
   componentWillMount() {
     var needed_tag = this.props.reqTag
+    var needed_year_start = this.props.reqStartYear
+    var needed_year_end = this.props.reqEndtYear
+    var start_month_range = this.props.startMonthRange
+    var start_year_range  = this.props.startYearRange
+    var end_month_range   = this.props.endMonthRange
+    var end_year_range    = this.props.endYearRange
     var filter_fn = function(elem) {
-      var flag = true
-      if(needed_tag && !elem.tags.includes(needed_tag))
-        flag = false
-      return flag
+      if ((needed_tag && !elem.tags.includes(needed_tag)) || 
+          (needed_year_start && elem.startYear !== needed_year_start) ||
+          (needed_year_end && elem.startYear !== needed_year_end)
+         )
+        return false
+      if (start_month_range && start_year_range && end_month_range && end_year_range) {
+        var startA = new Date(start_year_range, start_month_range - 1)
+        var endA = new Date(end_year_range, end_month_range - 1)
+        
+        var startB = new Date(elem.startYear, elem.startMonth - 1)
+        var endB = new Date(elem.endYear, elem.endMonth - 1)
+        if (!(startA < endB && endA >= startB))
+          return false
+      }
+      return true
     }
-    this.setState({data: require('./activity-feed.json').filter(filter_fn)})
+    this.setState({data: all_data.filter(filter_fn)})
   }
   
   render() {
     var elems = this.state.data
     var rows = numberRange(0, Math.round(elems.length / 2)).map(idx => idx * 2 + 1 < elems.length ? [elems[idx * 2], elems[idx * 2 + 1]] : [elems[idx * 2]])
     
-    console.log(rows)
     const rowstyle = {
       padding: '0',
     }
@@ -45,10 +76,10 @@ class DataList extends Component {
     return (
       <div>
         {rows.map(projs => 
-          <div className="row">
+          <div className="row" key={projs.map(proj => proj.title).join()}>
             <div className="col-lg-12" style={rowstyle}>
               {projs.map(proj =>
-              <Project title={proj.title}
+              <Project key={proj.title} title={proj.title}
                 date={getStringDate(proj.startMonth, proj.startYear,
                                     proj.endMonth, proj.endYear)}
                 link={proj.link}
--- src/Honors.js
+++ src/Honors.js
@@ -10,6 +10,7 @@ import Knee from './secondyear/Knee'
 import Japan from './secondyear/Japan'
 import YearTwoReview from './secondyear/SecondYearReview'
 import './App.css';
+import DataList from './DataList'
 
 class Honors extends Component {
   render() {
@@ -48,21 +49,7 @@ const List = function () {
       Second Year In Review
     </h2></NavLink>
     
-    <div className="col-lg-12" style={rowstyle}>
-      <Project title="Japan Trip" date="(Spring 2018)"
-        link="/UHP/Japan" icon={require('./media/icons/japanIcon.svg')} iconalt="Japan Icon">
-        After my coop at Infinera, I traveled to Japan for a study tour with 
-        other UC students for two weeks. I had fully recovered from my injury 
-        and was ready for a new experience. 
-      </Project>
-      <Project title="Accessibility Project" date="(Fall/Spring 2017,2018)"
-        link="/UHP/Knee" icon={require('./media/icons/stairIcon.png')} iconalt="Stair Icon">
-        In fall of last year, I tore my meniscus doing everyday activities
-        and needed to get surgery to repair the injury. This experinece is me 
-        reflecting upon the experience and the effect it has had on me. 
-      </Project>
-      
-    </div>
+    <DataList reqTag="Honors" startMonthRange={8} startYearRange={2017} endMonthRange={8} endYearRange={2018}/>
 
     <h2 className="text-center center-block col-lg-12">
       First Year
@@ -72,29 +59,9 @@ const List = function () {
       First Year In Review
     </h2></NavLink>
 
-    <div className="col-lg-12" style={rowstyle}>
-      <Project title="Treachery" date="(Spring/Summer 2017)"
-        link="/UHP/treachery" icon={require('./media/icons/lampIcon.png')} iconalt="Treachery Lamp Icon">
-        For my second honors experience, I worked with some of my fellow
-        students to create a video game based on the board game Betrayal at
-        House on the Hill.
-      </Project>
-      <Project title="Bearcat Coders" date="(Fall 2016 Spring 2017)"
-        link="/UHP/BearcatCoders" icon={require('./media/icons/bearcatCoders.png')} iconalt="Bearcat Coders Icon">
-      For my first Honors Experience, I decided to take a larger involvement
-      in the Association for Computing Machinery, Women's Chapter (ACM-W) and
-      their program to help teach coding to local high school students at Hughes
-      STEM High School.
-      </Project>
-    </div>
-    <div className="col-lg-12" style={rowstyle}>
-      <Project title="Gateway to University Honors" date="(Fall 2016)"
-        link="/UHP/intro" icon={require('./media/icons/UHP.png')} iconalt="UHP Icon">
-        As part of the honors curriculum, I had to take the gateway to
-        honors course. This is the introductory course to University Honors
-        Program at UC. To read more about my experience, click the link above.
-      </Project>
-    </div>
+    
+    <DataList reqTag="Honors" startMonthRange={8} startYearRange={2016} endMonthRange={8} endYearRange={2017}/>
+
 
     <div className="row"/>
     </div>
